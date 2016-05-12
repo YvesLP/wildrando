a74b7a8b9b83516eeecbcc26bb66ef9b9095239d
YvesLP <lepetitcorps_yves@yahoo.fr>
2016-05-12 16:48:10 +0200

    FOS User

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+// src/AppBundle/Entity/User.php
+/**
+ * Created by PhpStorm.
+ * User: yves
+ * Date: 12/05/16
+ * Time: 11:26
+ */
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
