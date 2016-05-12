496c2a1250ee9a84a81407261981440b75bdd4c8
topdeveloppement <topdeveloppement@gmail.com>
2016-05-12 16:26:23 +0200

    installation bootstrap api google

--- src/AppBundle/Controller/ShowApiController.php
+++ src/AppBundle/Controller/ShowApiController.php
@@ -6,15 +6,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class ShowApiController extends Controller
 {
     /**
-     * @Route("/", name="/")
+     * @Route("/homepage", name="homepage")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        return $this->render('homepage/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
